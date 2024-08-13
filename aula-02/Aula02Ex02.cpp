1fc71b23534a34b8577c662ae1b501a69b7bf009
Fabio Lopes <fabiolopes7844@gmail.com>
2024-08-12 22:04:15 -0300

    first commit

---
+++ aula-02/Aula02Ex02.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+
+bool obterPrimeiraDisponibilidade(int inicios[], int fins[], int quantidade,
+int duracao, int dataMaxima, int* inicio, int& fim) {
+  
+  for(int i = 0; i < quantidade; i++)
+  {
+    
+  }
+    
+
+    
+}
+
+
+/*Comente a main para enviar*/
+int main() {
+
+  int inicios[] = {1, 10, 13};
+  int fins[] = {3, 13, 15};
+  int quantidade = 3;
+  int duracao = 3;
+  int dataMaxima = 20;
+
+
+
+  return 0;
+}
