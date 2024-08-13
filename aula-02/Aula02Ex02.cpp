331a6db24984d2b2df4ee8022d092fd50549f42c
Fabio Lopes <fabiolopes7844@gmail.com>
2024-08-13 10:19:34 -0300

    update code

--- aula-02/Aula02Ex02.cpp
+++ aula-02/Aula02Ex02.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 
-
+using namespace std;
 
 bool obterPrimeiraDisponibilidade(int inicios[], int fins[], int quantidade,
 int duracao, int dataMaxima, int* inicio, int& fim) {
   
+  
   for(int i = 0; i < quantidade; i++)
   {
-    
-  }
-    
+    if(fins[i] <= dataMaxima - duracao && inicios[i+1] >= fins[i] + duracao){
+      
+      
+      cout << fins[i] << endl;
 
+      inicio = &fins[i];
+      fim = fins[i] + duracao;
+
+      cout << fins[i] + duracao << endl;
+
+      return true;
     
+    }
+
+
+
+  }
+  inicio = 0;
+  fim = 0;
+  return false;
 }
 
 
@@ -24,7 +40,14 @@ int main() {
   int duracao = 3;
   int dataMaxima = 20;
 
+  int* inicio;
+  int fim;
 
+  bool teste;
+
+  teste = obterPrimeiraDisponibilidade(inicios, fins, quantidade, duracao, dataMaxima, inicio, fim);
+
+  cout << teste;
 
   return 0;
 }
