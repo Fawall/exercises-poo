091feaaa1c20f0d809ff5f320a7637f434d48f52
Fabio Lopes <fabiolopes7844@gmail.com>
2024-09-16 13:34:18 -0300

    teste criado

--- aula-05/teste.cpp
+++ aula-05/teste.cpp
@@ -22,5 +22,30 @@ void teste1() {
 }
 
 void teste2() {
+
+    Hotel *h = new Hotel(10, 20);
+
+    Quarto *q = new Quarto(25, 4, 2);
+    Quarto *q2 = new Quarto(32,2,1);
+    Quarto *q3 = new Quarto(40,3,2);
+    
+    Reserva *r = new Reserva(q,2, 30);
+    Reserva *r2 = new Reserva(q2,10,12);
+    Reserva *r3 = new Reserva(q3,1,9);
+
+    h->adicionar(q);
+    h->adicionar(q2);
+    h->adicionar(q3);
+
+    h->fazer(r);
+    h->fazer(r2);
+    h->fazer(r3);
+
+    h->cancelar(r);
+
+    h->imprimir();
+    h->~Hotel();
+
+
     // IMPLEMENTE seguindo o enunciado
 }
