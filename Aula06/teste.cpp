8f4f1bd64d50f042ab6b4b459074432e1cb84de7
Fabio Lopes <fabiolopes7844@gmail.com>
2024-09-23 13:26:45 -0300

    Refactor code to add printing functionality for reservations and weekend packages

--- Aula06/teste.cpp
+++ Aula06/teste.cpp
@@ -1,6 +1,18 @@
-//Faca os includes necessarios
+#include <iostream>
+#include "Quarto.h"
+#include "Reserva.h"
+#include "PacoteDeFinalDeSemana.h"
 
 void teste1() {
+    Quarto* quarto = new Quarto(25,3,2);
+    Reserva* reserva = new Reserva(quarto, 2, 4);
+
+    PacoteDeFinalDeSemana* pacote = new PacoteDeFinalDeSemana(quarto, 5, true);
+
+    reserva ->imprimir();
+    pacote->imprimir();
+  
+    
 }
 
 void teste2() {
