c7523c357c8bea7fac172971dc641a7b8b84225c
Fabio Lopes <fabiolopes7844@gmail.com>
2024-09-23 14:18:17 -0300

    Add file associations for C++ headers in VSCode settings.json

--- Aula06/teste.cpp
+++ Aula06/teste.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Quarto.h"
 #include "Reserva.h"
+#include "Hotel.h"
 #include "PacoteDeFinalDeSemana.h"
 
 void teste1() {
@@ -16,5 +17,29 @@ void teste1() {
 }
 
 void teste2() {
+    
+    Quarto* quarto = new Quarto(25,3,2);
+    Reserva* reserva = new Reserva(quarto, 2, 4);
+    PacoteDeFinalDeSemana* pacote = new PacoteDeFinalDeSemana(quarto, 5, true);
+    reserva ->imprimir();
+    pacote->imprimir();
+
+    Hotel *hotel = new Hotel(10);
+    hotel->fazer(reserva);
+    hotel->fazer(pacote);
+
+    Quarto* quarto2 = new Quarto(32,4,2);
+    PacoteDeFinalDeSemana* pacote2 = new PacoteDeFinalDeSemana(quarto2, 7, true);
+    
+    hotel->fazer(pacote2);
+    hotel->imprimir();
+
+    reserva->~Reserva();
+    quarto->~Quarto();
+    quarto2->~Quarto();
+    hotel->~Hotel();    
 }
+
+
+
 
