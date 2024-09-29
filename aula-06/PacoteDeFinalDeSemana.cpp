fa9eb0019b52fc820186d2991b6a915c85f27229
Fabio Lopes <fabiolopes7844@gmail.com>
2024-09-29 19:28:35 -0300

    Refactor PacoteDeFinalDeSemana.cpp to fix calculation of total price when coffee is included

--- aula-06/PacoteDeFinalDeSemana.cpp
+++ aula-06/PacoteDeFinalDeSemana.cpp
@@ -6,14 +6,14 @@
 using namespace std;
 
 
-PacoteDeFinalDeSemana::PacoteDeFinalDeSemana(Quarto* quarto, int inicio, bool temCafe): Reserva(quarto,0, 2){
+PacoteDeFinalDeSemana::PacoteDeFinalDeSemana(Quarto* quarto, int inicio, bool temCafe): Reserva(quarto,inicio, inicio + 2){
 
     this->quarto = quarto;
     this->inicio = inicio;
-    this->temCafe = temCafe;
-    this->diaria = fim - inicio;
-    
-    if(temCafe != false){
+    this->cafe = temCafe;
+
+
+    if( Reserva::temCafe() != false){
         this->cafe = true;
         this->precoTotal = precoTotal + 20;
     } 
