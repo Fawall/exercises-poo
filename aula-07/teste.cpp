572f3e2d5d5bee5024bd54518e7f1b71a7f05cd4
Fabio Lopes <fabiolopes7844@gmail.com>
2024-09-29 18:08:40 -0300

    Refactor main.cpp to update test function calls

--- aula-07/teste.cpp
+++ aula-07/teste.cpp
@@ -1,9 +1,34 @@
-// Faça os includes necessários
+#include "QuartoDeLuxo.h"
+#include "Hotel.h"
+#include <iostream>
 
 void teste1() {
+    QuartoDeLuxo *quartoLuxo = new QuartoDeLuxo(18, true, false);
+    QuartoDeLuxo *quartoLuxo2 = new QuartoDeLuxo(19, true, true);
+    QuartoDeLuxo *quartoLuxo3 = new QuartoDeLuxo(20, false, false);
+    
+    quartoLuxo->imprimir();
+    quartoLuxo2->imprimir();
+    quartoLuxo3->imprimir();
     // Implemente segundo o enunciado
 }
 
 void teste2() {
+    Hotel *hotel = new Hotel(2, 2);
+    int quantidade = 0;
+    hotel->getQuartosDeLuxo(quantidade);
+    
+    Quarto *quarto = new Quarto(20, 4, 2);
+    hotel->adicionarQuarto(quarto);
+    hotel->getQuartosDeLuxo(quantidade);
+    cout << quantidade << endl;
+    
+    QuartoDeLuxo *quartoLuxo = new QuartoDeLuxo(21, true, true);
+    hotel->adicionarQuarto(quartoLuxo);
+    hotel->getQuartosDeLuxo(quantidade);
+    cout << quantidade << endl;
+ 
+
+    delete hotel;
     // Implemente segundo o enunciado
 }
