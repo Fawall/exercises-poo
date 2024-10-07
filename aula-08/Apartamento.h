a8f673fc42d4efb7a4e81938769c677d23779407
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-07 13:33:28 -0300

    Refactor Apartamento class to inherit from Acomodacao and add static methods for base price

--- aula-08/Apartamento.h
+++ aula-08/Apartamento.h
@@ -1,10 +1,15 @@
 #ifndef APARTAMENTO_H
 #define APARTAMENTO_H
 
+#include "Acomodacao.h"
 // Faça os includes necessários
 
-class Apartamento
-{
+class Apartamento : public Acomodacao {
+protected:
+  int numero;
+  double area;
+  static double precoBase;
+
 public:
   Apartamento(int numero, double area);
   ~Apartamento();
@@ -12,6 +17,10 @@ public:
   // Lembre de implementar os métodos não implementados
   double getArea();
   double getPrecoDiaria();
+  
+  static double getPrecoBase();
+  static void setPrecoBase(double precoBase);
+  
   void imprimir();
 };
 #endif
