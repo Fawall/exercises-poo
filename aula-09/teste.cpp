fa38a3ee97d6c5247f5584aa238f6263f1fbb0a0
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-20 16:18:32 -0300

    Refactor code to handle out of space errors and validate room and bed numbers

--- aula-09/teste.cpp
+++ aula-09/teste.cpp
@@ -1,4 +1,5 @@
 #include "Quarto.h"
+#include "SemEspaco.h"
 #include <iostream>
 #include <stdexcept>
 
@@ -7,22 +8,33 @@ using namespace std;
 void teste1() {
   try {
     Quarto quarto1(0, 1);
-  } catch (invalid_argument e) {
-    cout << e.what() << endl;
+  } catch (invalid_argument *e) {
+    cout << e->what() << endl;
     delete &e;
   }
 
   try {
     Quarto quarto2(2, -1);
-  } catch (invalid_argument e) {
-    cout << e.what() << endl;
+  } catch (invalid_argument *e) {
+    cout << e->what() << endl;
     delete &e;
   }
 
 }
 
 void teste2() {
-  // Altere conforme o enunciado
+  Quarto *q1 = new Quarto(1, 2);
+
+  q1->imprimir();
+
+  try {
+    q1->getPrecoDiaria(5);
+  } catch (SemEspaco *e) {
+    cout << e->what();
+    delete e;
+  } 
+
+  q1->~Quarto();
 }
 
 void teste3() {
