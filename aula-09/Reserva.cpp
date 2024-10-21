696b91468cc54ba72c04e0268a9e698470d54cf4
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-21 11:01:37 -0300

    Refactor Reserva.cpp to handle invalid arguments and check for room availability

--- aula-09/Reserva.cpp
+++ aula-09/Reserva.cpp
@@ -1,11 +1,27 @@
 // Faca os includes necessarios
 #include "Reserva.h"
+#include "SemEspaco.h"
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
 Reserva::Reserva(Quarto *quarto, int inicio, int fim, int numeroDePessoas) {
-  // Altere conforme o enunciado
+  if(inicio >= fim)
+    throw new invalid_argument("Duracao invalida");
+  if(numeroDePessoas < 1)
+    throw new invalid_argument("Numero de pessoas invalido");
+  try {
+    preco = quarto->getPrecoDiaria(numeroDePessoas);
+  } catch (SemEspaco *e) {
+    throw new invalid_argument("Quarto sem espaco");
+  }
+  
+  this->quarto = quarto;
+  this->inicio = inicio;
+  this->fim = fim;
+  this->numeroDePessoas = numeroDePessoas;
+
 }
 
 Reserva::~Reserva() {}
