8721f927463fbc53b278dcdc886352b24cac7285
Fabio Lopes <fabiolopes7844@gmail.com>
2024-10-28 12:34:27 -0300

    Refactor code to add persistence functionality for Quarto objects

--- aula-10/teste.cpp
+++ aula-10/teste.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 void teste1() {
-
+  int quantidade = 0;
   Quarto *q1 = new Quarto(1, 4);
   Quarto *q2 = new Quarto(2, 2);
   Quarto *q3 = new Quarto(3, 3);
@@ -19,7 +19,6 @@ void teste1() {
   p->inserir(q3);
 
 
-
   delete q1;
   delete q2;
   delete q3;
@@ -28,17 +27,49 @@ void teste1() {
 }
 
 void teste2() {
-  // implemente
+  PersistenciaDeQuarto *p = new PersistenciaDeQuarto("teste2.txt");
+ 
+  try{
+    int quantidade = 0;
+    Quarto **quartos = p->obter(quantidade);
+    for(int i = 0; i < quantidade; i++){
+      if(quartos[i] != nullptr){
+        quartos[i]->imprimir();
+      }
+    }
+
+  } catch(invalid_argument *e){
+    cout << e->what() << endl;
+    delete e;
+  } catch(logic_error *e){
+    cout << e->what() << endl;
+    delete e;
+  }
 }
 
-void teste3(){
-  
-  
-  ofstream arquivo;
+void testeJudge1(){
+  PersistenciaDeQuarto *p;
+  cout << "Inserir um quarto num arquivo vazio" << endl;
 
-  arquivo.open("teste.txt", ios::app);
-  arquivo << "teste" << endl;
+  Quarto *q1 = new Quarto(1, 3);
+  p = new PersistenciaDeQuarto("testeJudge1.txt");
+  p->inserir(q1);
 
-  arquivo.close();
+  Quarto *q2 = new Quarto(2, 2);
+  cout << "Inserir mais de um Quarto em arquivo vazio " << endl;
+  p = new PersistenciaDeQuarto("testeJudge1-2.txt");
+  p->inserir(q1);
+  p->inserir(q2);
 
+  cout << "Inserir mais de um quarto em um arquivo nao vazio" << endl;
+  Quarto *q3 = new Quarto(3,3);
+  Quarto *q4 = new Quarto(4,1);
+  p->inserir(q3);
+  p->inserir(q4);
+
+  delete p;
+  delete q1;
+  delete q2;
+  delete q3;
+  delete q4;
 }
