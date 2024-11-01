c8cd96ff16250ba03159060c6c90861033c0c857
Fabio Lopes <fabiolopes7844@gmail.com>
2024-11-01 15:47:14 -0300

    Implement test function for Hotel and Quarto classes

--- aula-11/teste.cpp
+++ aula-11/teste.cpp
@@ -1,7 +1,20 @@
-// Faca os includes necessarios
+#include "Hotel.h"
+#include "Quarto.h"
 
 void teste1() {
-  // implemente
+  Hotel* h = new Hotel();
+  Quarto *q1 = new Quarto(1);
+
+  h->adicionar(q1);
+
+  Quarto *q2 = new Quarto(2);
+  h->adicionar(q2);
+
+  h->imprimir();
+
+  h->~Hotel();
+  q1->~Quarto();
+  q2->~Quarto();
 }
 
 void teste2() {
